1976997ba4c25aa2bbcaad2a0afc2dc7ced5ae6d
Laurent Martins <laurent.martins@outlook.com>
2025-06-08 21:15:57 +0200

    :recycle: refacto

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Log from "./components/Log.jsx";
 import {WINNING_COMBINATIONS} from "./WINNING_COMBINATIONS.js";
 import GameOver from "./components/GameOver.jsx";
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -19,22 +24,7 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    X : 'Player 1',
-    O : 'Player 2',
-  })
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])];
-  for (const turn of gameTurns) {
-    const { square, player } = turn; //destructuration
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-
+function derivedWinner(gameBoard, players) {
   let winner;
   for (const combinations of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combinations[0].row][combinations[0].column];
@@ -47,7 +37,26 @@ function App() {
       winner = players[firstSquareSymbol]; //acces a la properties de l'objet
     }
   }
+  return winner;
+}
 
+function derivedGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
+  for (const turn of gameTurns) {
+    const { square, player } = turn; //destructuration
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  let gameBoard = derivedGameBoard(gameTurns);
+  const winner = derivedWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -80,19 +89,19 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName='Player 1'
+            initialName={PLAYERS.X}
             symbol='X'
             isActive={activePlayer === 'X'}
             onChangeName={handlePlayerNameChange}
           />
           <Player
-            initialName='Player 2'
+            initialName={PLAYERS.O}
             symbol='O'
             isActive={activePlayer === 'O'}
             onChangeName={handlePlayerNameChange}
           />
         </ol>
-        { (winner || hasDraw) && <GameOver winner={winner} onRestart={handleRematch}/>}
+        { (winner || hasDraw) && <GameOver winner={winner} onRestart={handleRematch}/> }
         <GameBoard
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
