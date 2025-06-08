3682430c369061219a43eaf7f91a09addd940838
Laurent Martins <laurent.martins@outlook.com>
2025-06-08 18:47:53 +0200

    :recycle: Supression du state active player

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import GameBoard from "./components/GameBoard.jsx";
 import {useState} from "react";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') { //check dernier tour jouer
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curentActivePlayer) => curentActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curentActivePlayer) => curentActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X') { //check du dernier tour joue
-        currentPlayer = 'O';
-      }
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       // updatedTurns
       return [
